68fa00f3720113afc6acc352a3a41f0816e89643
langung102 <minhlangwalker@gmail.com>
2023-11-12 13:07:04 +0700

    update override

--- main/override.h
+++ main/override.h
@@ -8,8 +8,8 @@
 // #undef configUSE_PREEMPTION
 // #define configUSE_PREEMPTION            1
 
-// #undef configUSE_TIME_SLICING
-// #define configUSE_TIME_SLICING          1
+#undef configUSE_TIME_SLICING
+#define configUSE_TIME_SLICING          0
 
 // #undef configIDLE_SHOULD_YIELD
 // #define configIDLE_SHOULD_YIELD         0
